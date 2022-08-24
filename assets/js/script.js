e425157700f3bc23d6909c4140338b931283d1e7
vomkaraxioned <omkarv@axioned.com>
2022-08-24 19:06:07 +0530

    css changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,16 @@
 
 */
 
+//form validation 
+let fname = document.forms['sign-in']['fname'];
+let lname = document.forms['sign-in']['lname'];
+let email = document.forms['sign-in']['email'];
+let phone = document.forms['sign-in']['phone'];
+let email = document.forms['sign-in']['email'];
+let btn = document.querySelector('input[type=submit');
 
+btn.addEventListener('click', validate);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function validate() {
+    console.log('i am clicked');
+}
