bbc1c1beb8a42d763020de0b54b01a4e7040bf9d
vomkaraxioned <omkarv@axioned.com>
2022-08-26 18:54:13 +0530

    simple changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,49 @@
 /* Author: 
 
 */
+const form = document.querySelector(".sign-in-form");
+const error = document.getElementsByClassName("err");
+class validator {
+    errorMsg = [];
+    constructor(values) {
+        values.forEach((val, i) => {
+            try {
+                if (val == "") {
+                    throw "this is required";
+                } else {
+                    throw "";
+                }
+            } catch (error) {
+                this.errorMsg[i] = error;
+            }
+        });
+        this.display();
+    }
+    display() {
+        alert(this.errorMsg);
+        this.errorMsg.forEach((msg, i) => {
+            alert(msg);
+            if (msg != "") {
+                console.log(error[i]);
+                error[i].innerHTML = msg;
+                error[i].style.textIndent = 0;
+            } else {
+                error[i].innerHTML = "";
+            }
+        });
+    }
 
+}
 //form validation 
-let fname = document.forms['sign-in']['fname'];
-let lname = document.forms['sign-in']['lname'];
-let email = document.forms['sign-in']['email'];
-let phone = document.forms['sign-in']['phone'];
-let email = document.forms['sign-in']['email'];
-let btn = document.querySelector('input[type=submit');
+form.addEventListener('submit', validate);
 
-btn.addEventListener('click', validate);
 
-function validate() {
-    console.log('i am clicked');
+function validate(e) {
+    const fname = document.forms['sign-in']['fname'].value;
+    const lname = document.forms['sign-in']['lname'].value;
+    const email = document.forms['sign-in']['email'].value;
+    const phone = document.forms['sign-in']['phone'].value;
+    let fields = [fname, lname, email, phone];
+    const valid = new validator(fields);
+    e.preventDefault();
 }
